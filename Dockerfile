aa538067c8a7baa55102c78cc50468f717606447
José Vitor <jose.vitorw018@gmail.com>
2026-07-26 13:20:41 -0300

    https

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,19 @@ FROM node:20-alpine
 
 WORKDIR /app
 
+RUN apk add --no-cache openssl
+
+RUN mkdir -p /app/certs && \
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /app/certs/server.key \
+    -out /app/certs/server.cert \
+    -subj "/CN=fargate.local/O=DevOps/OU=API"
+
 COPY package*.json ./
 RUN npm install --omit=dev
 
 COPY . .
 
-EXPOSE 3000
+EXPOSE 443
 
 CMD ["node", "src/server.js"]
--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
 const { createServer } = require('./app');
 
-const port = process.env.PORT || 3000;
-const server = createServer();
+const port = process.env.PORT || 443;
+const requestHandler = createServer(); // Agora recebe a função (req, res)
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, '../certs/server.key')),
+  cert: fs.readFileSync(path.join(__dirname, '../certs/server.cert'))
+};
+
+const server = https.createServer(options, requestHandler);
 
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port} (HTTPS)`);
 });
